57e81247afc07bf97eea326d05abcdd82e17ded6
gravityc95 <gravityc95@gmail.com>
2019-05-02 20:50:11 +0900

    회원가입 까지

--- SignUp.php
+++ SignUp.php
@@ -15,39 +15,39 @@
   <section class="container" style="padding-top: 45px;">
       <p class="lead" style="font-size: 60px;">Make Your Own ID</p>
       <p class="lead" style="font-size: 14px; color: #4c4c4c;"><strong>* </strong> 란은 필수입니다.</p>
-      <form class="" action="SignUp.html" method="post">
+      <form class="" action="SignUpAction.php" method="post">
         <div class="form-group">
           <label for="ID">ID <strong>* </strong></label>
           <div class="row">
-            <div class="col-lg-11">
-            <input type="ID" class="form-control" placeholder="ID를 입력하세요" autofocus>
+            <div class="col-lg-10">
+            <input type="text" name="ID" class="form-control" placeholder="ID를 입력하세요" autofocus>
             </div>
-            <div class="col-lg-1">
+            <div class="col-lg-2">
             <button type="submit" class="btn btn-info btn-sm" style="margin-top: 5px;">중복확인</button>
             </div>
           </div>
         </div>
         <div class="form-group">
           <label for="PW">PW <strong>* </strong></label>
-          <input type="PW" class="form-control" placeholder="비밀번호를 입력하세요">
+          <input type="text" name="PW" class="form-control" placeholder="비밀번호를 입력하세요">
         </div>
         <div class="form-group">
           <label for="PW_Confirm">PW 확인 <strong>* </strong></label>
-          <input type="PW_Confirm" class="form-control" placeholder="비밀번호를 다시 한 번 입력하세요">
+          <input type="text" name="PW_Confirm" class="form-control" placeholder="비밀번호를 다시 한 번 입력하세요">
         </div>
         <div class="form-group">
           <label for="Address">주소</label>
-          <input type="Address" class="form-control" placeholder="주소를 입력하세요">
+          <input type="text" name="Address" class="form-control" placeholder="주소를 입력하세요">
         </div>
         <div class="form-group">
           <label for="PhoneNumber">핸드폰 번호</label>
-          <input type="PhoneNumber" class="form-control" placeholder="핸드폰 번호를 입력하세요">
+          <input type="text" name="PhoneNumber" class="form-control" placeholder="핸드폰 번호를 입력하세요">
         </div>
         <button type="submit" class="btn btn-primary btn-lg" style="margin-top: 20px;">제출</button>
       </form>
   </section>
 
-  <footer id="Copyright" class="bg-dark mt-4 p-5 text-center fixed-bottom"> &copy; 2019 웹프로그래밍 </footer>
+  <footer id="Copyright" class="bg-dark mt-4 p-5 text-center"> &copy; 2019 웹프로그래밍 </footer>
 
   <!-- 제이쿼리 자바스크립트 추가하기 -->
   <script src="./js/jquery-3.2.1.min.js"></script>
---
+++ SignUpAction.php
@@ -0,0 +1,99 @@
+<?php
+  // DB 연결
+  $database_host = "localhost";
+  $database_user = "root";
+  $database_password = "ttdance4902";
+  $database_name = "";
+
+  $connect_object = mysqli_connect($database_host, $database_user, $database_password, $database_name);
+  if(mysqli_connect_error($connect_object)){
+    echo "MySQL 접속 오류";
+    echo "오류 원인 : ", mysqli_connect_error();
+    exit();
+  }
+
+  // Post 방식으로 유저 데이터를 가져옴
+  $ID = $_POST["ID"];
+  $PW = $_POST["PW"];
+  $PW_Confirm = $_POST["PW_Confirm"];
+  $Address = $_POST["Address"];
+  $PhoneNumber = $_POST["PhoneNumber"];
+
+  // 비밀번호가 비밀번호 확인과 맞지 않는 경우 에러처리
+  if($PW_Confirm != $PW){
+    echo ("<script language=javascript>alert('비밀번호가 비밀번호 확인과 맞지 않습니다.')</script>");
+    exit();
+  }
+
+  // 필수 입력 란이 비어 있으면 에러 처리
+  if($ID == "" || $PW == "" || $PW_Confirm = ""){
+    echo ("<script language=javascript>alert('필수 입력 칸이 비어 있습니다.')</script>");
+  }
+
+  $useDB = "
+    USE chattingdb;
+  ";
+
+  $ret = mysqli_query($connect_object, $useDB);
+
+  // DB에서 PK (ID) 중복 검사
+  $searchUserID = "
+    SELECT * FROM usersinfotbl
+  ";
+
+  $ret = mysqli_query($connect_object, $searchUserID);
+
+  /////////////////////////////////////////////
+  /////////////// For Dubugging ///////////////
+  /////////////////////////////////////////////
+
+  // if($ret){
+  //   echo mysqli_num_rows($ret), "건이 조회됨.<br><br>";
+  // }
+  // else{
+  //   echo "실패 원인: " .mysqli_error($connect_object);
+  //   exit();
+  // }
+
+  while($row = mysqli_fetch_array($ret)){
+    if($ID == $row['ID']){
+      echo ("<script language=javascript>alert('중복된 ID가 있습니다.')</script>");
+      echo ("<script>location.href='SignUp.php';</script>");
+      break;
+    }
+  }
+
+  $insertData = "
+    Insert INTO usersinfotbl (
+        ID,
+        PW,
+        Address,
+        PhoneNumber,
+        SignupDate
+      ) VALUES(
+        '$ID',
+        '$PW',
+        '$Address',
+        '$PhoneNumber',
+        Now()
+      )";
+
+  $ret = mysqli_query($connect_object, $insertData);
+
+  /////////////////////////////////////////////
+  /////////////// For Dubugging ///////////////
+  /////////////////////////////////////////////
+
+  // if($ret){
+  //   echo "성공";
+  // }
+  // else{
+  //   echo "실패 원인: " .mysqli_error($connect_object);
+  //   exit();
+  // }
+  echo ("<script language=javascript>alert('회원가입이 완료되었습니다!')</script>");
+  echo ("<script>location.href='SignUp.php';</script>");
+
+  mysqli_close($connect_object);
+
+?>
